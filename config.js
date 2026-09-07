09d978268200c1f850f380b818bcf26a77b8fb30
restforeverything2025-eng <rest.for.everything.2025@gmail.com>
2026-09-07 04:05:56 +0300

    Connect App order client to Render Core

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const Config = {
     Never place a secret or signing key in the frontend.
     The production URL will be supplied here when Core is deployed.
     */
-    coreOrderApiUrl: ""
+    coreOrderApiUrl: "https://previa-core.onrender.com"
 
 };
 
